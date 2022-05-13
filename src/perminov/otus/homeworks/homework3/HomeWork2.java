eadefa0b8ace837b1eaf53fcc222ac7e4a1598be
Vitalii Perminov <gruver2007@ya.ru>
2022-05-14 01:20:25 +0500

    change-homework2

--- src/perminov/otus/homeworks/homework3/HomeWork2.java
+++ src/perminov/otus/homeworks/homework3/HomeWork2.java
@@ -9,17 +9,17 @@ public class HomeWork2 {
         Scanner scanner = new Scanner(System.in);
         int answerUser, countCorrectAnswer = 0;
 
-        String[][][] questions = {
-                {{"Какая столица России?"},{"Москва","Пермь","Воронеж"}, {"1"}},
-                {{"Куда впадает Волга?"},{"Черное море","Тихий океан","Каспийское море"},{"3"}},
-                {{"Вокруг чего вращается земля?"},{"Юпитер","Солнце","Всё вращается вокруг земли"},{"2"}}
+        Object[][][] questions = {
+                {{"Какая столица России?"},{"Москва","Пермь","Воронеж"}, {1}},
+                {{"Куда впадает Волга?"},{"Черное море","Тихий океан","Каспийское море"},{3}},
+                {{"Вокруг чего вращается земля?"},{"Юпитер","Солнце","Всё вращается вокруг земли"},{2}}
         };
 
-        for (String[][] question: questions) {
-                    System.out.println(String.format("Вопрос: %s", question[0]));
-                    String[] answers = question[1];
+        for (Object[][] question: questions) {
+                    System.out.printf("Вопрос: %s", question[0]);
+                    Object[] answers = question[1];
                     System.out.println("---Варианты ответов: ---");
-                    for (String answer : answers) {
+                    for (Object answer : answers) {
                         System.out.println(answer);
                     }
                     System.out.println("Введите номер ответа:");
@@ -29,11 +29,11 @@ public class HomeWork2 {
                         System.out.println("Введён символ, а не цифра, программа завершена. Пожалуйста, начните сначала.");
                         break;
                     }
-                    if (Integer.parseInt(question[2][0]) == answerUser) {
+                    if ((Integer) question[2][0] == answerUser) {
                         countCorrectAnswer++;
                     }
         }
-        System.out.println(String.format("Количество правильных ответов: %s", countCorrectAnswer));
+        System.out.printf("Количество правильных ответов: %s", countCorrectAnswer);
         scanner.close();
     }
 }
